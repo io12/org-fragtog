6ff110f1c4f83ae2ef719079b0e17643076059d0
conao3 <conao3@gmail.com>
2020-02-11 06:07:12 +0900

    add spaces after period, and do fill-paragraph

--- org-fragtog.el
+++ org-fragtog.el
@@ -9,9 +9,10 @@
 
 ;;; Commentary:
 
-;; This package automates toggling org-mode latex fragment previews. Fragment
-;; previews are disabled for editing when your cursor steps onto them, and
-;; re-enabled when the cursor leaves.
+;; This package automates toggling org-mode latex fragment
+;; previews.  Fragment previews are disabled for editing when
+;; your cursor steps onto them, and re-enabled when the cursor
+;; leaves.
 
 ;;; Code:
 

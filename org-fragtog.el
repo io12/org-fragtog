2494bba45796ff93f84ebc2c07baaafcc6f4d1df
Benjamin Levy <blevy@protonmail.com>
2020-06-18 20:23:25 -0400

    Change major mode check to include derived modes (org-journal requires this)

--- org-fragtog.el
+++ org-fragtog.el
@@ -41,7 +41,7 @@
   "Toggle Org Latex Fragment Autotoggle Mode, a minor mode that automatically
 toggles org-mode latex fragment previews as the cursor enters and exits them"
   nil nil nil
-  (if (and org-fragtog-mode (eq major-mode 'org-mode))
+  (if (and org-fragtog-mode (derived-mode-p 'org-mode))
       (add-hook 'post-command-hook #'org-fragtog--post-cmd nil t)
     (remove-hook 'post-command-hook #'org-fragtog--post-cmd t)))
 
